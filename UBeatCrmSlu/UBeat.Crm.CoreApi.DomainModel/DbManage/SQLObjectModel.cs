155c311edc590187125b8e2afc06e566bbd0d815
钟冠辉 <zhongguanhui@renqiankeji.com>
2017-11-30 17:23:59 +0800

    开始处理数据库初始化配置管理功能

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.DomainModel/DbManage/SQLObjectModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.DomainModel.DbManage
+{
+    public class SQLObjectModel
+    {
+        public Guid Id { get; set; }
+        public SQLObjectTypeEnum ObjType { get; set; }
+        public string SqlPath { get; set; }
+        public string LastVersion { get; set; }
+        public string ObjName { get; set; }
+        public string Remark { get; set; }
+        public string RelativeObj { get; set; }
+        public string Name { get; set; }
+        public void checkEmpty() {
+            if (SqlPath == null) SqlPath = "";
+            if (LastVersion == null) LastVersion = "";
+            if (ObjName == null) ObjName = "";
+            if (Remark == null) Remark = "";
+            if (RelativeObj == null) RelativeObj = "";
+            if (Name == null) Name = "";
+        }
+    }
+    public class SQLTextModel {
+        public Guid Id { get; set; }
+        public Guid? SqlObjId { get; set; }
+        public string Version { get; set; }
+        public string Remark { get; set; }
+        public string SqlText { get; set; }
+        public SqlOrJsonEnum sqlOrJson { get; set; }
+        public InitOrUpdate initOrUpdate { get; set; }
+        public StructOrData structOrData { get; set; }
+        public int isRun { get; set; }
+        public void checkEmpty()
+        {
+            if (SqlObjId == null) SqlObjId = Guid.Empty;
+            if (Version == null) Version = "";
+            if (Remark == null) Remark = "";
+            if (SqlText == null) SqlText = "";
+        }
+
+
+    }
+    public class SQLReflectQueryModel {
+        public string[] RecIds { get; set; }
+    }
+    public enum SQLObjectTypeEnum {
+        All =0,
+        Table=1,
+        Func=2
+    }
+    public enum SqlOrJsonEnum {
+        All = 0,
+        SQL = 1,
+        Json =2
+    }
+    public enum InitOrUpdate {
+        All = 0,
+        Init = 1,
+        Update = 2
+    }
+    public enum StructOrData {
+        All = 0,
+        Struct = 1,
+        Data = 2
+    }
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi/Controllers/DbManageController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UBeat.Crm.CoreApi.DomainModel.DbManage;
+using UBeat.Crm.CoreApi.Services.Models;
+using UBeat.Crm.CoreApi.Services.Services;
+
+namespace UBeat.Crm.CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DbManageController : BaseController
+    {
+        private readonly DbManageServices _dbManageServices;
+        public DbManageController(DbManageServices dbManageServices) : base(dbManageServices) {
+            this._dbManageServices = dbManageServices;
+        }
+        [HttpPost("test")]
+        [AllowAnonymous]
+        public OutputResult<object> test() {
+            return null;
+            // return this._dbManageServices.GenerateTableCreateSQL("crm_sys_entity", UserId);
+            //return this._dbManageServices.GenerateProcSQL("crm_func_daily_add", UserId);
+        }
+
+        [HttpGet("export/platform/init/struct")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ExportPlatFormInitStructSQL() {
+            return null;
+        }
+        [HttpPost("reflect")]
+        [AllowAnonymous]
+        public OutputResult<object> reflect([FromBody] SQLReflectQueryModel paramInfo) {
+            if (paramInfo == null || paramInfo.RecIds == null || paramInfo.RecIds.Length == 0) {
+                return new OutputResult<object>("参数异常", "参数异常", -1);
+            }
+            return this._dbManageServices.ReflectInitStructSQL(paramInfo.RecIds, 0);
+        }
+
+    }
+}
