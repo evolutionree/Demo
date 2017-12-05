03e8dbb4c1fa62552a66fb17eb4e4331f1f4fed4
许彩辉 <xucaihui@renqiankeji.com>
2017-12-05 09:55:22 +0800

    修改实体功能列表保存接口

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/EntityProServices.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/EntityProServices.cs
@@ -559,7 +559,8 @@ namespace UBeat.Crm.CoreApi.Services.Services
                 info = new FunctionJsonInfo();
             }
             var entityInfo = _entityProRepository.GetEntityInfo(dynamicModel.EntityId);
-
+            if(entityInfo==null)
+                throw new Exception("实体数据不存在");
             webfuncs = info.WebFunctions;
             mobilefuncs = info.MobileFunctions;
             if (info.WebFunctions == null || info.WebFunctions.Count == 0)
@@ -578,7 +579,7 @@ namespace UBeat.Crm.CoreApi.Services.Services
         /// <returns></returns>
         public OutputResult<object> SaveFunctionList(SaveFuncsModel dynamicModel, int userNumber)
         {
-            throw new Exception("当前接口当前不开放");
+            
             if (dynamicModel == null)
                 throw new Exception("参数不可为空");
             if (dynamicModel.EntityId == Guid.Empty)
