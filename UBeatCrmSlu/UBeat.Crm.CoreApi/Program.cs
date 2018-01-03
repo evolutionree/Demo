94211c2d14557d6bb2819d2e978c70cd3c8b637d
钟冠辉 <zhongguanhui@renqiankeji.com>
2018-01-03 16:46:41 +0800

    优化定时事务

--- UBeatCrmSlu/UBeat.Crm.CoreApi/Program.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog;
+using Quartz;
+using Quartz.Impl;
+using UBeat.Crm.CoreApi.Utility;
 
 namespace UBeat.Crm.CoreApi
 {
     public class Program
     {
+        private static IScheduler _scheduler; 
         public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -29,7 +37,57 @@ namespace UBeat.Crm.CoreApi
                 .Build();
 
 
+            StartScheduler();//启动主Scheler
             host.Run();
+        }
+
+        private static void StartScheduler()
+        {
+            var config = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+            int isStart = 0;
+            IEnumerator<IConfigurationSection> it = config.GetSection("ScheduleSetting").GetChildren().GetEnumerator();
+            while (it.MoveNext())
+            {
+                IConfigurationSection item = it.Current;
+                if (item.Key.Equals("IsStart") && item.Value != null)
+                {
+                    int tmp = 0;
+                    if (Int32.TryParse(item.Value, out tmp))
+                    {
+                        isStart = tmp;
+                    }
+                }
+            }
+            if (isStart != 1)
+            {
+                Logger logger = LogManager.GetLogger("UBeat.Qrtz");
+                logger.Warn("!!!!!未配置后台事务启动，后台事务将不在本服务器内运行!!!!!!");
+                return;
+            }
+            var properties = new NameValueCollection
+            {
+            };
+
+            var schedulerFactory = new StdSchedulerFactory(properties);
+            _scheduler = schedulerFactory.GetScheduler().Result;
+            _scheduler.Start().Wait();
+
+            var mainScheduleJob = JobBuilder.Create<MainSchedulerJobImp>()
+                .WithIdentity("MainScheduleJob")
+                .Build();
+            var mainScheduleTrigger = TriggerBuilder.Create()
+                .WithIdentity("MainScheduleCron")
+                .StartNow()
+                .WithCronSchedule("* * * * * ?")
+                .Build();
+            //每秒调用一次
+
+            _scheduler.ScheduleJob(mainScheduleJob, mainScheduleTrigger).Wait();
+
         }
     }
 }
