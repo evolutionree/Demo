b3976ed5e6fa37388e2f324eeb43bad1c4e573fb
sqicoo <sqicoo@126.com>
2018-01-17 17:01:43 +0800

    增加审批可见规则保存

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Models/Rule/RuleModel.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Models/Rule/RuleModel.cs
@@ -15,13 +15,14 @@ namespace UBeat.Crm.CoreApi.Services.Models.Rule
     /// </summary>
     public class RuleModel
     {
-        public int TypeId { get; set; }//0 代表菜单 1代表角色 2代表动态实体 3代表分支流程规则
+        public int TypeId { get; set; }//0 代表菜单 1代表角色 2代表动态实体 3代表分支流程规则 
         public string Id { get; set; }// 代表规则关联的某实体唯一id
         public string RoleId { get; set; }
         public string MenuName { get; set; }
         public string RuleId { get; set; }
         public string RuleName { get; set; }
         public string EntityId { get; set; }
+        public string FlowId { get; set; }
         public string RelEntityId { get; set; }
         public string Rulesql { get; set; }
         public ICollection<RuleItemModel> RuleItems { get; set; }
