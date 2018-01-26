d4fe74a71df97f04fc3433e3a10860127ba1a7df
sqicoo <sqicoo@126.com>
2018-01-26 15:43:04 +0800

    完成缩略图

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Utility/ThumbnailHelper.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Utility/ThumbnailHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using UBeat.Crm.CoreApi.DomainModel.FileService;
@@ -160,9 +161,10 @@ namespace UBeat.Crm.CoreApi.Services.Utility
             Stream stream = new MemoryStream(sourcedata);
             original_image = new Bitmap(stream);
             // Calculate the new width and height
-            int original_width = original_image.Width;//截取原图宽度
-            int original_height = original_image.Height;//截取原图高度
-            if (fileType == 1) {
+            double original_width = original_image.Width;//截取原图宽度
+            double original_height = original_image.Height;//截取原图高度
+            if (fileType == 1)
+            {
                 long imageQuality = 50L;
                 Encoder myEncoder = Encoder.Quality;
                 EncoderParameters myEncoderParameters = new EncoderParameters(1);
@@ -173,6 +175,87 @@ namespace UBeat.Crm.CoreApi.Services.Utility
                 //Directory.CreateDirectory(cachePath);
                 original_image.Save(cachePath, myImageCodecInfo, myEncoderParameters);
                 original_image.Save(ms, myImageCodecInfo, myEncoderParameters);
+            }
+            else if (fileType == 2) {
+                //聊天图片 标准4:3 或者3:4   超过4:2居中截图
+                double multiple_x = 2;//横缩放截图阈值
+                double multiple_y = 0.5;//竖缩放截图阈值
+                //方向
+                Boolean orientation = (original_width / original_height)>1?true:false;
+                int newWidth = 0;
+                int newHeight = 0;
+                if (orientation)
+                {
+                    //横方向
+                    if ((original_width / original_height) > multiple_x)
+                    {
+                        //截图
+                        if (original_height <= 300)
+                        {
+                            newWidth = (int)(original_height / 0.75);
+                            newHeight = (int)original_height;
+                        }
+                        else {
+                            double multiple = Math.Round(original_height / 300, 2);
+                            newWidth = (int)(original_height / 0.75/multiple);
+                            newHeight = 300;
+                        }
+                        ms = zoomBitmap(original_image, cachePath, newWidth, newHeight);
+                    }
+                    else {
+                        //小于400原图返回
+                        if (original_width <= 400)
+                        {
+                            ms = GetMultipleStream(original_image, cachePath, original_width, original_height, 1);
+                        }
+                        else {
+                             double multiple = Math.Round(original_width / 400, 2);
+                             ms = GetMultipleStream(original_image, cachePath, original_width, original_height, multiple);
+                        }
+                    }
+                }
+                else {
+                    if ((original_width / original_height)< multiple_y)
+                    {
+                        //截图
+                        if (original_width <= 300)
+                        {
+                            newWidth = (int)original_width;
+                            newHeight =(int)(original_width / 0.75);
+                        }
+                        else
+                        {
+                            double multiple = Math.Round(original_width / 300, 2);
+                            newWidth = 300;
+                            newHeight = (int)(original_width / 0.75 / multiple); ;
+                        }
+                        ms = zoomBitmap(original_image, cachePath, newWidth, newHeight);
+                    }
+                    else
+                    {
+                        //小于400原图返回
+                        if (original_height <= 400)
+                        {
+                            ms = GetMultipleStream(original_image, cachePath, original_width, original_height, 1);
+                        }
+                        else {
+                            double multiple = Math.Round(original_height / 400, 2);
+                            ms = GetMultipleStream(original_image, cachePath, original_width, original_height, multiple);
+                        }
+                    }
+                }
+            }
+            else if (fileType == 3)
+            {
+                double final_width = 160;
+                //缩略比例
+                double multiple = 1;
+                //头像标准160*160
+                if (original_width > 160)
+                {
+                    multiple = Math.Round(original_width / final_width, 2);
+                }
+                ms=GetMultipleStream(original_image,cachePath,original_width,original_height,multiple);
             }
 
             try
@@ -194,9 +277,28 @@ namespace UBeat.Crm.CoreApi.Services.Utility
                     final_image.Dispose();
                 if (original_image != null) original_image.Dispose();
                 if (thumbnail_image != null) thumbnail_image.Dispose();
+                if (ms != null) ms.Dispose();
             }
         }
         #endregion
+
+        //// <summary>
+        /// 按缩放比返回
+        /// </summary>
+        private static MemoryStream GetMultipleStream(Bitmap original_image, string cachePath, double original_width, double original_height, double multiple)
+        {
+            MemoryStream ms = new MemoryStream();
+            double final_width = (int)(original_width / multiple);
+            double final_height = (int)(original_height / multiple);
+            Bitmap newImage = new Bitmap((int)final_width, (int)(final_height));
+            using (Graphics graphic = Graphics.FromImage(newImage)) {
+                graphic.DrawImage(original_image, 0, 0, (int)final_width, (int)final_height);
+                newImage.Save(cachePath, ImageFormat.Jpeg);
+                newImage.Save(ms, ImageFormat.Jpeg);
+            }
+            return ms;
+        }
+
         //// <summary>
         /// 获取图片编码信息
         /// </summary>
@@ -212,6 +314,56 @@ namespace UBeat.Crm.CoreApi.Services.Utility
                 }
             }
             return null;
+        }
+
+        /** 
+     * 按宽/高缩放图片到指定大小并进行裁剪得到中间部分图片 <br> 
+     * 方 法 名：zoomBitmap <br> 
+     * @param original_image 源bitmap 
+     * @param w 缩放后指定的宽度 
+     * @param h 缩放后指定的高度 
+     * @return 缩放后的中间部分图片 Bitmap 
+     */
+        public static MemoryStream zoomBitmap(Bitmap original_image,string cachePath, int w, int h)
+        {
+            MemoryStream ms = new MemoryStream();
+            int x = 0;
+            int y = 0;
+            int ow = original_image.Width;
+            int oh = original_image.Height;
+            if ((double)original_image.Width / (double)original_image.Height > (double)w / (double)h)
+            {
+                oh = original_image.Height;
+                ow = original_image.Height * w / h;
+                y = 0;
+                x = (original_image.Width - ow) / 2;
+            }
+            else
+            {
+                ow = original_image.Width;
+                oh = original_image.Width * h / w;
+                x = 0;
+                y = (original_image.Height - oh) / 2;
+            }
+            try
+            {
+                Bitmap newImage = new Bitmap(w,h);
+                using (Graphics graphic = Graphics.FromImage(newImage))
+                {
+                    graphic.Clear(Color.Transparent);
+                    graphic.DrawImage(original_image, new Rectangle(0, 0, w, h),
+                                  new Rectangle(x, y, ow, oh),
+                                  GraphicsUnit.Pixel);
+                    newImage.Save(cachePath, ImageFormat.Jpeg);
+                    newImage.Save(ms, ImageFormat.Jpeg);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e.Message);
+                return null;
+            }
+            return ms;
         }
 
         /// <summary>
