45179a0de4b73d3e14d4870c24947f0906c70091
钟冠辉 <zhongguanhui@crmuke.com>
2018-05-18 00:12:40 +0800

    【WebIM】正在处理WebIM，完成登陆接口和发送消息接口

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/webchat/WebChatHandler.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/webchat/WebChatHandler.cs
@@ -9,24 +9,37 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UBeat.Crm.CoreApi.Core.Utility;
+using UBeat.Crm.CoreApi.Services.Models.Chat;
 using UBeat.Crm.CoreApi.Services.Services;
 
 namespace UBeat.Crm.CoreApi.Services.webchat
 {
     public class WebChatHandler
     {
-      
+
         private readonly RequestDelegate _next;
+
+        private AccountServices _accountService = null;
+        private ChatServices _chatService = null;
+        private int UserId = 0;
         public WebChatHandler(RequestDelegate next)
         {
             _next = next;
         }
         public async Task Invoke(HttpContext context)
         {
+
             if (!context.WebSockets.IsWebSocketRequest)
             {
                 await _next.Invoke(context);
                 return;
+            }
+            if (_accountService == null)
+            {
+                _accountService = ServiceLocator.Current.GetInstance<AccountServices>();
+            }
+            if (_chatService == null) {
+                _chatService = ServiceLocator.Current.GetInstance<ChatServices>();
             }
             CancellationToken ct = context.RequestAborted;
             var currentSocket = await context.WebSockets.AcceptWebSocketAsync();
@@ -54,7 +67,18 @@ namespace UBeat.Crm.CoreApi.Services.webchat
                 }
 
 
-                string response = await ReceiveStringAsync(currentSocket, ct);
+                string response = null;
+                try
+                {
+                    response = await ReceiveStringAsync(currentSocket, ct);
+                    if (response != null && response.StartsWith("@heart")) continue;
+                }
+                catch (Exception ex) {
+                    //已经连接异常了，需要把服务删除
+                    WebSockChatSocketManager.getInstance().UnbindSocketWithUser(currentSocket, -1);
+                    break;
+                }
+
                 WebChatMsgTemplate msg = null;
                 try
                 {
@@ -80,8 +104,10 @@ namespace UBeat.Crm.CoreApi.Services.webchat
                 };
                 switch (msg.Cmd) {
                     case WebChatCommandType.WebChatLogin:
+                        Login(msg, currentSocket);
                         break;
                     case WebChatCommandType.WebChatLogout:
+                        ReceiveMsg(msg, currentSocket);
                         break;
                     case WebChatCommandType.WebChatListMsg:
                         break;
@@ -102,28 +128,156 @@ namespace UBeat.Crm.CoreApi.Services.webchat
                 WebChatResponseHandler.getInstance().Enqueue(pkg);
 
             }
-            await currentSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", ct);
-            currentSocket.Dispose();
+            try
+            {
+
+                await currentSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", ct);
+            }
+            catch (Exception ex) {
+            }
+            try
+            {
+                currentSocket.Dispose();
+            }
+            catch (Exception ex) {
+            }
         }
 
-        
-        public void Login(WebChatMsgTemplate cmd)
+        /// <summary>
+        /// 登陆处理方法
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="websock"></param>
+        public void Login(WebChatMsgTemplate cmd, WebSocket websock)
         {
-            AccountServices _service = ServiceLocator.Current.GetInstance<AccountServices>();
+            WebChatResponseMsg responseMsg = null;
+            WebResponsePackage pkg = null;
             try
             {
                 Dictionary<string, object> data = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(cmd.Data));
                 int userid = 0;
                 string authcode = "";
-                if (data.ContainsKey("userid")) {
-
+                if (data.ContainsKey("userid") && data["userid"] != null) {
+                    userid = int.Parse(data["userid"].ToString());
                 }
-            }catch(Exception ex)
+                if (data.ContainsKey("authorizedcode") && data["authorizedcode"] != null)
+                {
+                    authcode = data["authorizedcode"].ToString();
+                }
+                if (userid <= 0 || authcode.Length == 0) {
+                    //发送登陆失败的回复
+                    responseMsg = new WebChatResponseMsg()
+                    {
+                        ResultCode = -2,
+                        ErrorMsg = "登陆失败(code:-2)"
+                    };
+                    pkg = new WebResponsePackage()
+                    {
+                        WebSock = websock,
+                        CmdMsg = responseMsg,
+                        MessageType = WebChatMsgType.Command//命令回复数据
+                    };
+                    WebChatResponseHandler.getInstance().Enqueue(pkg);
+                    return;
+                }
+                if (_accountService.CheckAuthorizedCodeValid(userid, authcode))
+                {
+                    WebSockChatSocketManager.getInstance().MapSocketToUser(websock, userid);
+                    UserId = userid;
+                    //发送登陆成功的消息
+                    responseMsg = new WebChatResponseMsg()
+                    {
+                        ResultCode = 0,
+                        ErrorMsg = "登陆成功"
+                    };
+                    pkg = new WebResponsePackage()
+                    {
+                        WebSock = websock,
+                        CmdMsg = responseMsg,
+                        MessageType = WebChatMsgType.Command//命令回复数据
+                    };
+                    WebChatResponseHandler.getInstance().Enqueue(pkg);
+                }
+                else {
+                    //发送登陆失败的回复
+                    responseMsg = new WebChatResponseMsg()
+                    {
+                        ResultCode = -2,
+                        ErrorMsg = "登陆失败(code:-3)"
+                    };
+                    pkg = new WebResponsePackage()
+                    {
+                        WebSock = websock,
+                        CmdMsg = responseMsg,
+                        MessageType = WebChatMsgType.Command//命令回复数据
+                    };
+                    WebChatResponseHandler.getInstance().Enqueue(pkg);
+                }
+            }
+            catch (Exception ex)
             {
-
+                responseMsg = new WebChatResponseMsg()
+                {
+                    ResultCode = -2,
+                    ErrorMsg = "登陆失败(code:-4)"
+                };
+                pkg = new WebResponsePackage()
+                {
+                    WebSock = websock,
+                    CmdMsg = responseMsg,
+                    MessageType = WebChatMsgType.Command//命令回复数据
+                };
+                WebChatResponseHandler.getInstance().Enqueue(pkg);
             }
         }
 
+        /// <summary>
+        /// 发送消息功能
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="webSock"></param>
+        public void ReceiveMsg(WebChatMsgTemplate cmd, WebSocket webSock) {
+            WebChatResponseMsg responseMsg = null;
+            WebResponsePackage pkg = null;
+            try
+            {
+                SendChatModel model = Newtonsoft.Json.JsonConvert.DeserializeObject<SendChatModel>(JsonConvert.SerializeObject(cmd.Data));
+                if (model != null)
+                {
+                    _chatService.SendChat(model, UserId);
+                }
+                else {
+                    //发送消息异常的回复
+                    responseMsg = new WebChatResponseMsg()
+                    {
+                        ResultCode = -10,
+                        ErrorMsg = "发送消息失败"
+                    };
+                    pkg = new WebResponsePackage()
+                    {
+                        ReceiverId = UserId,
+                        CmdMsg = responseMsg,
+                        MessageType = WebChatMsgType.Command//命令回复数据
+                    };
+                    WebChatResponseHandler.getInstance().Enqueue(pkg);
+                }
+            }
+            catch (Exception ex) {
+                //发送消息异常的回复
+                responseMsg = new WebChatResponseMsg()
+                {
+                    ResultCode = -10,
+                    ErrorMsg = "发送消息失败"
+                };
+                pkg = new WebResponsePackage()
+                {
+                    ReceiverId = UserId,
+                    CmdMsg = responseMsg,
+                    MessageType = WebChatMsgType.Command//命令回复数据
+                };
+                WebChatResponseHandler.getInstance().Enqueue(pkg);
+            }
+        }
         private static async Task<string> ReceiveStringAsync(System.Net.WebSockets.WebSocket socket, CancellationToken ct = default(CancellationToken))
         {
             var buffer = new ArraySegment<byte>(new byte[8192]);
