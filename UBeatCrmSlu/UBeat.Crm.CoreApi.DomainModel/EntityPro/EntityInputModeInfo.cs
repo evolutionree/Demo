102a4bd7e6a54bda0601defb285cfb12401475f7
钟冠辉 <zhongguanhui@crmuke.com>
2018-06-10 13:47:27 +0800

    【实体定义】增加实体输入方式定义

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.DomainModel/EntityPro/EntityInputModeInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.DomainModel.EntityPro
+{
+    
+    public class EntityInputModeInfo
+    {
+        public EntityInputMethod InputMethod { get; set; }
+        public int RecStatus { get; set; }
+        public string Title { get; set; }
+        /// <summary>
+        /// 这个参数在两个地方生效,名片扫描及手机通讯录中
+        /// </summary>
+        public CardOcrParamInfo CarOrcParam { get; set; }
+    }
+    public enum EntityInputMethod {
+        CommonInput = 1 ,//普通录入
+        CardOcr = 2,//名片扫描录入
+        DeviceAddressBook=3//手机通讯录
+    }
+    public class CardOcrParamInfo {
+        public List<CarOcrMapFieldInfo> MapFields { get; set; }
+    }
+    public class CarOcrMapFieldInfo {
+        public string OcrFieldName { get; set; }
+        public string EntityFieldName { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Repository/sql/v7.3.2/2018-06-10/002_crm_sys_entity.sql
@@ -0,0 +1,1 @@
+﻿alter table crm_sys_entity add inputmethod jsonb;
