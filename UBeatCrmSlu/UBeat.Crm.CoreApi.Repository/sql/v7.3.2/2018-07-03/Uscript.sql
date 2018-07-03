7ca163948bbd5cee1fab54893dfd6aaafe7a9e9a
钟冠辉 <zhongguanhui@crmuke.com>
2018-07-03 19:00:40 +0800

    增加脚本【UScript】

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Repository/sql/v7.3.2/2018-07-03/Uscript.sql
@@ -0,0 +1,5 @@
+﻿alter table crm_sys_entity_extfunction add enginetype int4 not null default(1);
+
+alter table crm_sys_entity_extfunction add uscript text;
+
+update crm_sys_entity_extfunction set enginetype =1 ;
