ec8297ac742ff081bcb7355248a0db8e5b792957
朱志磊 <zhuzhilei@crmuke.com>
2018-07-13 11:16:49 +0800

    【添加实体或工作流--暂存处理】

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/WorkFlowServices.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/WorkFlowServices.cs
@@ -417,7 +417,9 @@ namespace UBeat.Crm.CoreApi.Services.Services
                 var tran = conn.BeginTransaction();
                 try
                 {
-
+                    Guid g = Guid.Parse(caseModel.CacheId);
+                    if (!string.IsNullOrEmpty(caseModel.CacheId) && !(_dynamicEntityRepository.ExistsData(g, userinfo.UserId, tran)))
+                        _dynamicEntityRepository.DeleteTemporary(g, userinfo.UserId, tran);
                     if (caseModel.DataType == 0)
                     {
                         var entityInfo = _entityProRepository.GetEntityInfo(caseModel.EntityModel.TypeId);
