2a816ce9e204b607f8a2bc5cc7d001ec28ceb2a4
朱志磊 <zhuzhilei@crmuke.com>
2018-07-16 17:55:41 +0800

    【多语言】

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Controller/Controllers/MoreLanguageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UBeat.Crm.CoreApi.Controllers;
+using UBeat.Crm.CoreApi.Services.Models;
+using UBeat.Crm.CoreApi.Services.Services;
+
+namespace UBeat.Crm.CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class MoreLanguageController : BaseController
+    {
+        [HttpPost]
+        [Route("morelanglist")]
+        public OutputResult<object> MoreLanguageList()
+        {
+            string[] language = { "CN-中文-GMT+8", "EN-英文-GMT", "TW-繁体-GMT+8" };
+            List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+            foreach (var item in language)
+            {
+                Dictionary<string, string> detail = new Dictionary<string, string>();
+                var msg = item.Split('-');
+                detail.Add("key", msg[0]);
+                detail.Add("dispaly", msg[1]);
+                detail.Add("gmt", msg[2]);
+                list.Add(detail);
+            }
+            return new OutputResult<object>(list);
+        }
+    }
+}
