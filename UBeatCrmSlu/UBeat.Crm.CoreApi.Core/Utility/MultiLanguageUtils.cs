f0d6c4276f397d71248022fe950911190a12b697
钟冠辉 <zhongguanhui@crmuke.com>
2018-07-18 17:56:51 +0800

    【多语言】提醒规则及回收规则接口多语言支持

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Core/Utility/MultiLanguageUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.Core.Utility
+{
+    public class MultiLanguageUtils
+    {
+        public static string DefaultLanguage = "CN";
+        public static string GetDefaultLanguageValue(Dictionary<string, object> multilanguage)
+        {
+            if (multilanguage == null) return null;
+            foreach (var item in multilanguage)
+            {
+                if (item.Key.ToUpper() == DefaultLanguage)
+                {
+                    if (item.Value == null) return null;
+                    else return item.Value.ToString();
+                }
+            }
+            return null;
+        }
+
+        public static string GetDefaultLanguageValue(Dictionary<string, string> multilanguage)
+        {
+            if (multilanguage == null) return null;
+            foreach (var item in multilanguage)
+            {
+                if (item.Key.ToUpper() == DefaultLanguage)
+                {
+                    if (item.Value == null) return null;
+                    else return item.Value.ToString();
+                }
+            }
+            return null;
+        }
+    }
+}
--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Models/Reminder/ReminderEvent.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Models/Reminder/ReminderEvent.cs
@@ -145,7 +145,8 @@ namespace UBeat.Crm.CoreApi.Services.Models.Reminder
         public int RepeatType { get; set; }
         public string CronString { get; set; }
         public string Remark { get; set; }
-        public string ReminderLanguage { get; set; }
+
+        public Dictionary<string, string> ReminderName_Lang { get; set; }
 
     }
 
