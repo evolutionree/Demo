770bd05effb6e8eb95e1bc1c48c8a8f3244b15b5
彭小锋 <pengxiaofeng@wxchina.com>
2018-11-20 11:16:14 +0800

    部门同步

--- UBeatCrmSlu/UBeat.Crm.CoreApi.DingTalk/Utils/DingTalkConfig.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.DingTalk/Utils/DingTalkConfig.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UBeat.Crm.CoreApi.Core.Utility;
-
+using System.Linq;
 namespace UBeat.Crm.CoreApi.DingTalk.Utils
 {
     public class DingTalkConfig
@@ -31,23 +31,35 @@ namespace UBeat.Crm.CoreApi.DingTalk.Utils
         public string AGENTId { get; set; }
         public string RootDeptId { get; set; }
         static IConfigurationSection config;
-
+        public List<MapperDeptId> mapperDeptIds { get; set; }
         private static DingTalkConfig instance = null;
         public static DingTalkConfig getInstance()
         {
             if (instance != null) return instance;
-            config = ServiceLocator.Current.GetInstance<IConfigurationRoot>().GetSection("DingdingConfig");
+            IConfigurationRoot configRoot = ServiceLocator.Current.GetInstance<IConfigurationRoot>();
+            var deptIds = configRoot.GetSection("DeptIdMapper").Get<List<MapperDeptId>>();
+            config = configRoot.GetSection("DingdingConfig");
             instance = new DingTalkConfig()
             {
                 AGENTId = config.GetValue<string>("AgentId"),
                 CorpSecret = config.GetValue<string>("CorpSecret"),
                 CorpId = config.GetValue<string>("CorpId"),
-                RootDeptId = config.GetValue<string>("RootDeptId"),
+                mapperDeptIds = deptIds,
+                // RootDeptId = config.GetValue<string>("RootDeptId"),
                 //SSOCorpSecret = "9Shab6LrfXPP_gaPpFx_jNbqTsjj2Ii-jt1BaL4nGSUskwjWNyA7Mu90sO_n9fU_",
                 //AppId = "dingoakvipp0zjcxb5mm2q",
                 //AppSecret = "OFS2koE1MpdJdzd54kt1RRimwpnDVm7kLYv9OPU5Rz6fBQvvyQHPcNHd5CsHSOB-",
             };
+
             return instance;
         }
+
+
+    }
+
+    public class MapperDeptId
+    {
+        public String DingDingDeptId { get; set; }
+        public String CrmDeptId { get; set; }
     }
 }
