96f5b3e77bb9a523982c9f7b8f5c291ed556e641
lip <1475977946@qq.com>
2018-11-29 19:19:20 +0800

    修复手机端列表进入报错问题
    Signed-off-by: lip <1475977946@qq.com>

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/DynamicEntityServices.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/DynamicEntityServices.cs
@@ -1959,7 +1959,7 @@ namespace UBeat.Crm.CoreApi.Services.Services
             //}
             //else
             //    WhereSQL = " e.recstatus = 1   ";
-            if (dynamicEntity.SearchData.Count() > 0)
+            if (dynamicEntity.SearchData != null &&  dynamicEntity.SearchData.Count() > 0)
                 WhereSQL += " and e." + dynamicEntity.SearchData.Keys.FirstOrDefault() + "  like '%" + dynamicEntity.SearchData.Values.FirstOrDefault() + "%'";
             string innerSQL = string.Format(@"select {0} from {1}  where  {2} order by {3} limit {4} offset {5}",
                 selectClause, fromClause, WhereSQL, OrderBySQL, pageParam.PageSize, (pageParam.PageIndex - 1) * pageParam.PageSize);
