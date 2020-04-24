ab6952fb315cb518d94c3d9043fc39dbece80840
彭小锋 <pengxiaofeng@wxchina.com>
2020-04-24 17:31:08 +0800

    企信宝接口

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.IRepository/IDockingAPIRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UBeat.Crm.CoreApi.DomainModel;
+
+namespace UBeat.Crm.CoreApi.IRepository
+{
+    public interface IDockingAPIRepository
+    {
+        OperateResult InsertBussinessInfomation(BussinessInformation data, int userNumber);
+        List<BussinessInformation> GetBussinessInfomation(string selectField, int isLike, string companyName, int userNumber);
+        OperateResult UpdateBussinessInfomation(BussinessInformation data, int userNumber);
+    }
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Repository/Repository/BussinessInformation/DockingAPIRepository.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UBeat.Crm.CoreApi.DomainModel;
+using UBeat.Crm.CoreApi.IRepository;
+using UBeat.Crm.CoreApi.Repository.Utility;
+
+namespace UBeat.Crm.CoreApi.Repository.Repository
+{
+    public class DockingAPIRepository : RepositoryBase, IDockingAPIRepository
+    {
+        public OperateResult InsertBussinessInfomation(BussinessInformation data, int userNumber)
+        {
+            var executeSql = @"insert into crm_sys_bussiness_infomation(basicinfo,yearreport,casedetail,lawsuit,courtnotice,breakpromise,companyname,reccreator,recupdator) values (@basicinfo::jsonb,@yearreport::jsonb,@casedetail::jsonb,@lawsuit::jsonb,@courtnotice::jsonb,@breakpromise::jsonb,@companyname,@reccreator,@recupdator)";
+
+            var existSql = " select count(1) from crm_sys_bussiness_infomation where companyname=@companyname;";
+            var args = new
+            {
+                basicinfo = data.BasicInfo,
+                yearreport = data.YearReport,
+                lawsuit = data.LawSuit,
+                courtnotice = data.CourtNotice,
+                breakpromise = data.BreakPromise,
+                casedetail = data.CaseDetail,
+                companyname = data.CompanyName,
+                reccreator = userNumber,
+                recupdator = userNumber
+            };
+            var result = DataBaseHelper.QuerySingle<int>(existSql, args);
+            if (result == 1)
+            {
+                this.UpdateBussinessInfomation(data, userNumber);
+                return new OperateResult { Flag = 1 };
+            }
+
+            return DataBaseHelper.QuerySingle<OperateResult>(executeSql, args);
+        }
+        public OperateResult UpdateBussinessInfomation(BussinessInformation data, int userNumber)
+        {
+            var executeSql = @"  update  crm_sys_bussiness_infomation set {0},recupdator=@recupdator   where companyname=@companyname;";
+            DynamicParameters args = new DynamicParameters();
+            args.Add("companyname", data.CompanyName);
+            args.Add("recupdator", userNumber);
+            string condition = string.Empty;
+            if (!string.IsNullOrEmpty(data.BasicInfo))
+            {
+                args.Add("basicinfo", data.BasicInfo);
+                condition = " basicinfo=@basicinfo ";
+            }
+            if (!string.IsNullOrEmpty(data.YearReport))
+            {
+                args.Add("yearreport", data.YearReport);
+                condition = " yearreport=@yearreport ";
+            }
+            if (!string.IsNullOrEmpty(data.CaseDetail))
+            {
+                args.Add("lawsuit", data.CaseDetail);
+                condition = " casedetail=@casedetail ";
+            }
+            if (!string.IsNullOrEmpty(data.LawSuit))
+            {
+                args.Add("courtnotice", data.LawSuit);
+                condition = " lawsuit=@lawsuit ";
+            }
+            if (!string.IsNullOrEmpty(data.CourtNotice))
+            {
+                args.Add("courtnotice", data.CourtNotice);
+                condition = " courtnotice=@courtnotice ";
+            }
+            if (!string.IsNullOrEmpty(data.BreakPromise))
+            {
+                args.Add("breakpromise", data.BreakPromise);
+                condition = " breakpromise=@breakpromise ";
+            }
+
+            return DataBaseHelper.QuerySingle<OperateResult>(executeSql, args);
+        }
+
+        public List<BussinessInformation> GetBussinessInfomation(string selectField, int isLike, string companyName, int userNumber)
+        {
+            //ILIKE '%' || @keyword || '%' ESCAPE '`'
+            var executeSql = @" select {0} from  crm_sys_bussiness_infomation where companyname {1} ";
+            string condition = string.Empty;
+            if (isLike == 0)
+                condition = " ILIKE '%' || @keyword || '%' ESCAPE '`' ";
+            else
+                condition = " = @keyword";
+            executeSql = string.Format(executeSql, selectField, condition);
+
+            var args = new
+            {
+                keyword = companyName
+            };
+
+            return DataBaseHelper.Query<BussinessInformation>(executeSql, args);
+        }
+    }
+}
