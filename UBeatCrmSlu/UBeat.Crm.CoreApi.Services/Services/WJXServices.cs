85f52950038a1ae28a6ff4f81fa5857740e244a0
彭小锋 <pengxiaofeng@wxchina.com>
2020-05-22 16:26:58 +0800

    问卷星

--- UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/WJXServices.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.Services/Services/WJXServices.cs
@@ -26,7 +26,7 @@ namespace UBeat.Crm.CoreApi.Services.Services
         {
             var config = _configurationRoot.GetSection("WJXConfig").Get<WJXSSOConfigModel>();
             var stamp = GetTimeStamp();
-            string ssoUrl = string.Format(config.SSOUrl, config.AppId, config.APPkey, config.User, SignatureHelper.Sha1Signature(config.AppId +config.APPkey +config.User + stamp));
+            string ssoUrl = string.Format(config.SSOUrl, config.AppId, config.APPkey, config.User, SignatureHelper.Sha1Signature(config.AppId + config.APPkey + config.User + stamp));
             return new OutputResult<object>(ssoUrl);
         }
 
@@ -34,7 +34,8 @@ namespace UBeat.Crm.CoreApi.Services.Services
         {
             try
             {
-                if (entityId==null||entityId == Guid.Empty) {
+                if (entityId == null || entityId == Guid.Empty)
+                {
                     throw new Exception("实体id不能为空");
                 }
                 DynamicEntityDetailtMapper entityModel = new DynamicEntityDetailtMapper();
@@ -44,24 +45,29 @@ namespace UBeat.Crm.CoreApi.Services.Services
                 var resultData = _dynamicEntityServices.Detail(entityModel, 1);
                 IDictionary<string, object> detailData = null;
                 if (resultData != null && resultData.ContainsKey("Detail") && resultData["Detail"] != null && resultData["Detail"].Count > 0)
-                    detailData=resultData["Detail"][0];
+                    detailData = resultData["Detail"][0];
 
-                if (detailData == null) {
+                if (detailData == null)
+                {
                     throw new Exception("没有找到对应实体记录");
                 }
                 var config = _configurationRoot.GetSection("WJXConfig").Get<WJXSSOConfigModel>();
                 var stamp = GetTimeStamp();
+                //sign=sha1(appid+appkey+username+joiner+activity+joinid+realname+dept+extf+ts)
+                string sign = SignatureHelper.Sha1Signature(config.AppId + config.APPkey + config.User + "20200000168" + "73334626" + "3" + stamp);
+                string sign1 = SignatureHelper.Sha1Signature(config.AppId + config.APPkey+ "73334626" + stamp);
                 string qUrl = string.Format(config.QUrl, config.AppId, config.User, stamp, SignatureHelper.Sha1Signature(config.AppId + config.APPkey + config.User + stamp));
                 Task<String> taskResult = DingdingMsgService.GetJson(qUrl, null, null);
                 taskResult.Wait();
                 var result = taskResult.Result;
-                List<WJXQuestionModel> searchResultList=new List<WJXQuestionModel>();
-                if (result != null) {
+                List<WJXQuestionModel> searchResultList = new List<WJXQuestionModel>();
+                if (result != null)
+                {
                     searchResultList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<WJXQuestionModel>>(result);
 
                     foreach (var question in searchResultList)
                     {
-                        question.qurl = "https://www.wjx.cn/jq/" + question.qid + ".aspx?sojumpparm="+ detailData["reccode"].ToString();
+                        question.qurl = "https://www.wjx.cn/jq/" + question.qid + ".aspx?sojumpparm=" + detailData["reccode"].ToString();
                     }
                 }
                 return new OutputResult<object>(searchResultList);
