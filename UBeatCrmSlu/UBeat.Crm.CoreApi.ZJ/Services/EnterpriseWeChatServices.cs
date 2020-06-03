340bc54ddb5cc171e2c1debf23fc56362176d6a0
彭小锋 <pengxiaofeng@wxchina.com>
2020-06-03 09:57:41 +0800

    中京企业微信单点登录

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZJ/Services/EnterpriseWeChatServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UBeat.Crm.CoreApi.DomainModel;
+using UBeat.Crm.CoreApi.Services.Services;
+using UBeat.Crm.CoreApi.ZJ.WJXModel;
+
+namespace UBeat.Crm.CoreApi.ZJ.Services
+{
+    public class EnterpriseWeChatServices : BasicBaseServices
+    {
+        public static readonly string OAuth2 = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
+        public OperateResult GetSSOCode(EnterpriseWeChatModel enterpriseWeChat)
+        {
+
+        }
+    }
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZJ/WJXModel/EnterpriseWeChatModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.ZJ.WJXModel
+{
+    public enum UrlTypeEnum
+    {
+        WorkFlow = 1,
+        SmartReminder = 2
+    }
+    public class EnterpriseWeChatModel
+    {
+        public UrlTypeEnum UrlType { get; set; }
+        public Dictionary<string, object> Data { get; set; }
+    }
+}
