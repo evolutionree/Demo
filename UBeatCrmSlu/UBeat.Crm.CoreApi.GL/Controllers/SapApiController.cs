7d4c7d6c276229c9c2d090a532bfc47f81f41cc3
sqicoo <sqicoo@gmail.com>
2020-12-08 15:07:17 +0800

    加入crm被sap调用验证接口

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.GL/Controllers/SapApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UBeat.Crm.CoreApi.Controllers;
+using UBeat.Crm.CoreApi.GL.Model;
+using UBeat.Crm.CoreApi.Services.Models;
+using UBeat.Crm.CoreApi.Services.Models.DynamicEntity;
+using UBeat.Crm.CoreApi.Services.Models.WJXModel;
+using UBeat.Crm.CoreApi.Services.Services;
+
+namespace UBeat.Crm.CoreApi.Controllers
+{
+
+    [Route("gl/[controller]")]
+    public class SapApiController : BaseController
+    {
+        private readonly DynamicEntityServices _dynamicEntityServices;
+
+        public SapApiController(DynamicEntityServices dynamicEntityServices)
+        {
+            _dynamicEntityServices = dynamicEntityServices;
+        }
+
+        [Route("test")]
+        [HttpPost]
+        [AllowAnonymous]
+        public OutputResult<object> SapTest([FromBody] SynSapModel paramInfo = null)
+        {
+            if (paramInfo == null|| paramInfo.type==-1)
+            {
+                return ResponseError<object>("参数异常");
+            }
+            var typeId = (BizSynEnum)Convert.ToInt32(paramInfo.type);
+            switch (typeId)
+            {
+                case BizSynEnum.验证业务:
+                        return new OutputResult<object>("测试通过:参数:" + typeId.ToString());
+                    break;
+                default:
+                        return ResponseError<object>("没有找到对应业务类型");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.GL/Model/SapModelInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UBeat.Crm.CoreApi.GL.Model
+{
+    public class SapModelInfo
+    {
+        public SapModelInfo()
+        {
+        }
+    }
+
+    public class SynSapModel
+    {
+        public SynSapModel()
+        {
+            type = -1;
+        }
+        public int type { get; set; }
+        public Guid EntityId { get; set; }
+        public List<Guid> RecIds { get; set; }
+        public Dictionary<string, object> OtherParams { get; set; }
+    }
+
+    public enum BizSynEnum
+    {
+        None = 0,
+        验证业务 = 1,
+    }
+
+    public class SynResultModel
+    {
+        public SynResultModel()
+        {
+            Result = false;
+            Message = string.Empty;
+        }
+        public bool Result { get; set; }
+        public string Message { get; set; }
+    }
+
+    public enum DataSourceType
+    {
+        SAP = 1,
+        CRM = 2,
+    }
+
+    public class AutoSynSapModel
+    {
+        public Guid EntityId { get; set; }
+        public Guid RecId { get; set; }
+    }
+}
