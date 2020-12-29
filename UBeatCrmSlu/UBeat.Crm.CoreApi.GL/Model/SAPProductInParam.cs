9714a010af0752a41edc852af5776dd2ee5e898a
彭小锋 <pengxiaofeng@wxchina.com>
2020-12-29 10:53:56 +0800

    产品代码提交

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.GL/Model/SAPProductInParam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.GL.Model
+{
+    public class SAPProductInParam
+    {
+        public int TYPE { get; set; }
+        public SAPProduct DATA { get; set; }
+    }
+    public class SAPProduct
+    {
+        public String GUID { get; set; }
+        public String MODE { get; set; }
+        public List<Dictionary<String, String>> MARA { get; set; }
+        public List<Dictionary<String, String>> MAKT { get; set; }
+        public List<Dictionary<String, String>> MARM { get; set; }
+        public List<Dictionary<String, String>> MARC { get; set; }
+        public List<Dictionary<String, String>> MBEW { get; set; }
+        public List<Dictionary<String, String>> QMAT { get; set; }
+        public List<Dictionary<String, String>> MVKE { get; set; }
+        public List<Dictionary<String, String>> MLAN { get; set; }
+    }
+}
