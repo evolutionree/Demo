5cbf82af1186cba8180c840966ef52675f2cc04a
GerryZ <192048984@qq.com>
2021-01-16 17:54:43 +0800

    【银行信息】 补充

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.GL/Model/BankModelInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.GL.Model
+{
+   public class BankModelInfo
+    {
+        public BankModelInfo()
+        {
+        }
+    }
+
+
+    public class SapBankModelResult
+    {
+        public string TYPE { get; set; }
+        public string MESSAGE { get; set; }
+        public List<Dictionary<string,object>> DATA { get; set; }
+
+    }
+
+}
