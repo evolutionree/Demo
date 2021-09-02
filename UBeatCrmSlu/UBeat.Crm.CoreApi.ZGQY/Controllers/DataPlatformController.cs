cc5ad903638491345a4ccbdbdde24fa229199564
PC-202008141306\Administrator <zhaiqiuqiu@wxchina.com>
2021-09-02 11:33:38 +0800

    数据平台文件

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Controllers/DataPlatformController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UBeat.Crm.CoreApi.Controllers;
+using UBeat.Crm.CoreApi.Services.Models;
+using UBeat.Crm.CoreApi.ZGQY.Model;
+using UBeat.Crm.CoreApi.ZGQY.Services;
+
+namespace UBeat.Crm.CoreApi.ZGQY.Controllers
+{
+	[Route("api/zgqy/[controller]")]
+	public class DataPlatformController: BaseController
+	{
+		private static readonly Logger Logger = LogManager.GetLogger(typeof(AttendanceController).FullName);
+
+		private readonly DataPlatformServices _dataPlatformServices;
+
+		public DataPlatformController(DataPlatformServices dataPlatformServices)
+		{
+			_dataPlatformServices = dataPlatformServices;
+		}
+
+		[Route("test")]
+		[HttpPost]
+		[AllowAnonymous]
+		public OutputResult<object> SapTest([FromBody] DataPlatformInfo paramInfo = null)
+		{
+			if (paramInfo == null)
+			{
+				return ResponseError<object>("参数异常");
+			}
+			return new OutputResult<object>("test");
+		}
+	}
+}
---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Model/DataPlatformInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBeat.Crm.CoreApi.ZGQY.Model
+{
+	public class DataPlatformInfo
+	{
+	}
+
+
+}
