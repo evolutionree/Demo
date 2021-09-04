9728c9a7db3cdd8a76f30878b3ac536cd9e5434d
PC-202008141306\Administrator <zhaiqiuqiu@wxchina.com>
2021-09-04 09:57:44 +0800

    add file

---
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Repository/DataPlatformRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using UBeat.Crm.CoreApi.Repository.Repository;
+using UBeat.Crm.CoreApi.Repository.Utility;
+using UBeat.Crm.CoreApi.ZGQY.Model;
+
+namespace UBeat.Crm.CoreApi.ZGQY.Repository
+{
+	public class DataPlatformRepository: MiddleDbBaseRepository
+	{
+		public List<SaveProjectInfo> GetProejctData(DateTime date)
+		{
+			string sql = @"select * from cdmop_org_prj_project_contract_view_dd where CREATION_DATE::date >= @date";
+
+			var param = new DynamicParameters();
+			param.Add("date", date);
+
+			return DataBaseHelper.Query<SaveProjectInfo>(sql, param, CommandType.Text, ConnectionString);
+		}
+	}
+}
