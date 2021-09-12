9bd78eaa1ce203865a10e0ac7dca7fd519a8b9de
zhaiqiuqiu@wxchina.com <zhaiqiuqiu@wxchina.com>
2021-09-12 11:25:45 +0800

    同步数据平台： 开票，回款，招待，差旅

--- UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Model/DataPlatformInfo.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Model/DataPlatformInfo.cs
@@ -42,4 +42,77 @@ namespace UBeat.Crm.CoreApi.ZGQY.Model
 		public string RecName { get; set; }
 		public int Recmanager { get; set; }
 	}
+
+	public class SaveBillInfo
+	{
+		public SaveBillInfo()
+		{
+		}
+		public string paf_num { get; set; }
+		public string applicant { get; set; }
+		public string applicant_num { get; set; }
+
+		public string department { get; set; }
+		public string invoice_date { get; set; }
+		public string contract_code { get; set; }
+		public string money { get; set; }
+		public string paf_status { get; set; }
+		public string creation_date { get; set; }
+		public string paf_primary_id { get; set; }
+		 
+		public string contract_code_json { get; set; }
+	}
+
+	public class QueryUserInfo
+	{
+		public int UserId { get; set; }
+		public string UserName { get; set; }
+		public Guid DeptId { get; set; }
+		public string DeptName { get; set; }
+	}
+
+	public class SavePayInfo
+	{
+		public SavePayInfo()
+		{
+		}
+		public string bill_code { get; set; }
+		public string contract_code { get; set; }
+		public string receivable_date { get; set; }
+
+		public string distribute_amount { get; set; }
+		
+		public string contract_code_json { get; set; }
+	}
+
+	public class SaveZdInfo
+	{
+		public SaveZdInfo()
+		{
+		}
+		public string number { get; set; }
+		public string applier { get; set; }
+		public string applierNumber { get; set; }
+
+		public string orgUnit { get; set; }
+		public string entertain_date { get; set; }
+		public string amount { get; set; }
+		public string used_flag { get; set; }
+	}
+
+
+	public class SaveClInfo
+	{
+		public SaveClInfo()
+		{
+		}
+		public string number { get; set; }
+		public string applier { get; set; }
+		public string applierNumber { get; set; }
+
+		public string orgUnit { get; set; }
+		public string travel_date { get; set; }
+		public string amount { get; set; }
+		public string used_flag { get; set; }
+	}
 }
--- UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Repository/IDataPlatformRepository.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Repository/IDataPlatformRepository.cs
@@ -10,5 +10,18 @@ namespace UBeat.Crm.CoreApi.ZGQY.Repository
 		List<SaveProjectInfo> GetProejctData(DateTime date);
 		QueryModelInfo GetContractByCode(string code);
 		Guid IsExitProject(String code);
+
+		List<SaveBillInfo> GetBillData(DateTime date);
+		Guid IsExitBill(String code);
+		QueryUserInfo GetUserIdByWorkcode(string code);
+
+		List<SavePayInfo> GetPayData(DateTime date);
+		Guid IsExitPay(String code);
+
+		List<SaveZdInfo> GetZdData(DateTime date);
+		Guid IsExitZd(String code);
+
+		List<SaveClInfo> GetClData(DateTime date);
+		Guid IsExitCl(String code);
 	}
 }
