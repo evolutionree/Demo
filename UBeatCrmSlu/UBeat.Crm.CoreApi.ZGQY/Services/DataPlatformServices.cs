e158e2de9bfdadeb2329a0917c2b64b19c8ced88
冼烊宇 <xianyangyu@wxchina.com>
2021-11-04 11:41:57 +0800

    调整开票信息同步的开票合同号字段

--- UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Services/DataPlatformServices.cs
+++ UBeatCrmSlu/UBeat.Crm.CoreApi.ZGQY/Services/DataPlatformServices.cs
@@ -133,7 +133,7 @@ namespace UBeat.Crm.CoreApi.ZGQY.Services
 				var recmanager = userId;
 				if (contract != null)
 				{
-					item.contract_code_json = JsonHelper.ToJson(new DataSourceInfo() { id = contract.RecId, name = contract.RecName });
+					item.contract_code_json = JsonHelper.ToJson(new DataSourceInfo() { id = contract.RecId, name = item.contract_code });
 					recmanager = contract.Recmanager;
 				}
 				else
